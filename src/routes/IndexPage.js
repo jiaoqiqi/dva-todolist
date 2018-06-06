bc40a14aca31f960830799bce34f72ceaeec3d96
jiaoqiqi <1176932165@qq.com>
2018-06-06 22:40:14 +0800

    build event list without check
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import {connect} from 'dva';
-import {Layout} from 'antd';
+import {Layout, Button, Input, List, Checkbox} from 'antd';
 import styles from './IndexPage.css';
+
+const {Content} = Layout;
 
 class IndexPage extends React.Component {
 
   constructor(props, context) {
     super(props, context);
+    this.state = {
+      list: [
+        {
+          name: '買醬油',
+          status: true
+        }, {
+          name: '背單字',
+          status: false
+        }, {
+          name: '打咚咚',
+          status: true
+        }
+      ]
+    }
   }
 
   render() {
@@ -16,6 +32,30 @@ class IndexPage extends React.Component {
         <h1>TodoList
           <small>( Dva + AntD )</small>
         </h1>
+        <Content>
+          <Input placeholder="請輸入待辦事項"/>
+          <Button type="primary" icon="plus">新增</Button>
+
+          <List
+            className={styles.list}
+            bordered
+            dataSource={this.state.list}
+            renderItem={(item, index) => (
+              <List.Item>
+                <Checkbox
+                  className={(item.status ? styles.check : ' l')}
+                  checked={item.status}
+                >{item.name}</Checkbox>
+                <Button
+                  className={styles.btndel}
+                  type="danger"
+                  size="small"
+                  shape="circle"
+                  icon="cross" />
+              </List.Item>
+            )} />
+
+        </Content>
       </Layout>
     );
   }
